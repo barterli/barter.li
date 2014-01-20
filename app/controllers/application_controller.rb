e6396a6e6bb371c58bde8ff857345dc5cbd6b67b
suras <msurendar@outlook.com>
2014-01-20 13:06:24 +0530

    authentication devise changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, :except => [:create]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
