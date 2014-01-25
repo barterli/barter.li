f94257c85a6bf697c430efe4314a1aa654bbe5a0
suras <msurendar@outlook.com>
2014-01-25 22:24:18 +0530

    book model unit test update

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe Book do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+     @attr = {
+       :title => "Rails",
+       :author => "Dave",
+     }
+   end
+   it "should create Book with tile and author in lowercase" do
+     book = Book.create(@attr)
+     book.title.chr.ord.should_not eq("R".chr.ord)
+     book.title.chr.upcase.ord.should eq("R".chr.ord)
+   end
+   
+   it "should increase book visit count" do
+     book = Book.create(@attr)
+     book.book_visit_count
+     book.visits.should eq(1)
+   end
+
+   it "return book barter categories" do
+     categories = Book.barter_categories
+     categories.kind_of?(Hash).should be_true
+   end
+
+   it "records visitor to book" do
+    book = Book.create(@attr)
+    book.book_visit_user(1)
+    visit = UserBookVisit.last
+    visit.user_id.should eq(1)
+    visit.book_id.should eq(book.id)
+   end
 end
+
