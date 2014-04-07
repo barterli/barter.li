2b5285e57504454aa090d8fac273b0e8b449686c
suras <msurendar@outlook.com>
2014-04-07 12:39:24 +0530

    tag creation after_initialize issue

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -86,8 +86,11 @@ class Api::V1::BooksController < Api::V1::BaseController
   #    ```
   def create
     tag_ids = get_tag_ids
-    @book = current_user.books.new(book_params.merge(location_id: book_location, tag_ids: tag_ids))
+    @book = current_user.books.new(book_params.merge(location_id: book_location))
     if @book.save
+      # needed because of overriding tag_ids after initialize in module habtm_touch_id
+      @book.reload
+      @book.tag_ids = tag_ids
       WishListWorker.perform_async(@book.id)  # for background wishlist processing
       render json: @book 
     else
