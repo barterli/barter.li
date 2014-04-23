7fcc4cafc513058ca78e1f542deb4c595b077283
suras <msurendar@outlook.com>
2014-04-23 15:52:27 +0530

    user profile serializer change

--- app/serializers/user_profile_serializer.rb
+++ app/serializers/user_profile_serializer.rb
@@ -1,10 +1,18 @@
 class UserProfileSerializer < ActiveModel::Serializer
-  cached
-  attributes :id, :first_name, :last_name, :id_user, :profile_image, :location
+  # cached
+  attributes :id, :first_name, :last_name, :id_user, :location, :image_url, :description
+  has_many :books
 
   def profile_image
   	 url = @options[:url_options]
     "#{url[:protocol]}#{url[:host]}:#{url[:port]}#{object.profile_image}"
+  end
+
+  def image_url
+    return object.ext_image if object.ext_image.present?
+    url = @options[:url_options]
+    port = url[:port].present? ?  ":"+url[:port].to_s: ""
+    object.absolute_profile_image("#{url[:host]}#{port}")
   end
 
   def location
