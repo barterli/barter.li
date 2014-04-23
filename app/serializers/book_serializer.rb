5e6030322efeb476cf65830bff17a3d867624b27
suras <msurendar@outlook.com>
2014-04-23 15:38:01 +0530

    cap public upload symlink and tmp dir issue fix

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,6 +1,6 @@
 class BookSerializer < ActiveModel::Serializer
   #cached 
-  attributes :id, :title, :author, :publication_year, :publication_month, :value
+  attributes :id, :title, :author, :publication_year, :publication_month, :value,
              :image_url, :barter_type, :location, :tags, :id_book, :description, :isbn_10, :isbn_13, :id_user
   
   def location
--- config/deploy.rb
+++ config/deploy.rb
@@ -11,8 +11,13 @@ set :deploy_to, '/home/deploy/barter.li'
 # set :format, :pretty
 # set :log_level, :debug
 # set :pty, true
-
  set :linked_files, %w{config/database.yml config/application.yml}
+ set :linked_dirs, %w{bin log tmp/backup tmp/pids tmp/cache tmp/sockets vendor/bundle}
+ set :linked_dirs, fetch(:linked_dirs) + %w{public/system public/uploads}
+ # linked_dirs = Set.new(fetch(:linked_dirs, [])) # https://github.com/capistrano/rails/issues/52
+ # linked_dirs.merge(%w{bin log tmp/pids tmp/cache tmp/sockets public/system public/uploads})
+ # set :linked_dirs, linked_dirs.to_a
+
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -20,21 +25,16 @@ set :deploy_to, '/home/deploy/barter.li'
 
 namespace :deploy do
 
-  before "deploy:restart", :symlink_directories
-  task :symlink_directories do
-  execute :mkdir, "#{shared_path}/uploads" unless File.exists?("#{shared_path}/uploads")
-  run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
-  end
-
   desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-       execute :mkdir, release_path.join('tmp') unless File.exists?(release_path.join('tmp'))
-       execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+    task :restart do
+      on roles(:app), in: :sequence, wait: 5 do
+        # Your restart mechanism here, for example:
+         # execute :mkdir, release_path.join('tmp') unless File.exists?(release_path.join('tmp'))
+         execute :touch, release_path.join('tmp/restart.txt')
+      end
 
+  end
+  
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
